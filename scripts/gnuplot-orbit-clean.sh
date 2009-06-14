ec2799cd6ae8bc59f75f428b105b8044464de841
Paul Schulz <paul@mawsonlakes.org>
2009-06-14 23:58:45 +0930

    Added scripts to automatically generate plot

---
+++ scripts/gnuplot-orbit-clean.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# File: gnuplot-orbit-clean.sh
+# Generate orbital data and plot (cleanup script)
+#
+# Author: Paul Schulz <paul@mawsonlakes.org>
+# Licence: GPL
+
+# Removes created files.
+
+rm -r orbit.out
+rm -f orbit.xyz
+rm -f orbit.ps
